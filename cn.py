f9c3b40d7fb374d03e6479960c13656517d780fe
Xbnr23 <mecroia7re9@gmail.com>
2024-12-23 23:35:27 +0100

    Update cn.py

--- cn.py
+++ cn.py
@@ -8,7 +8,7 @@ cards = [
 ]
 
 # بوابة دفع وهمية
-url = "https://example-payment-gateway.com/validate_card"
+url = "https://checkout.stripe.com/c/pay/ppage_1QZJrbDOVUu6yhjNz3qN7Gt4#fidkdWxOYHwnPyd1blppbHNgWjAwdGNSNnNKMGo1SHZqalNGX2JQYjdpSzU1dXFoM2tQVEInKSdobGF2Jz9%2BJ2hwbGEnPydLRCcpJ3ZsYSc%2FJ0tEJyknYnBsYSc%2FJ0tEJ3gpJ2dgcWR2Jz9eWCknaWR8anBxUXx1YCc%2FJ3Zsa2JpYFpscWBoJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKm5qYWwrcXMnKSdpamZkaWAnP2twaWl4JSUl"
 
 # التحقق من كل بطاقة
 for card in cards:
